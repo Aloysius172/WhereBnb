8dfa2e389ed0d65dee20edf222ec6bb51c58e1c9
“Aloysius <aloysius.ekezie1@gmail.com>
2022-08-15 12:17:20 -0400

    working on User auth process

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+alo = User.create!(username: "alo1", email: "alo1@email.com", password: "alo123")
