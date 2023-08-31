0bb1c23f113cbec943a22b01026c357529c5efbd
“Aloysius <aloysius.ekezie1@gmail.com>
2023-08-31 02:06:02 -0400

    reservation form guest dropdown styling

--- frontend/components/reservations/reservation_form.jsx
+++ frontend/components/reservations/reservation_form.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
-import { IoIosArrowUp } from '@react-icons/all-files/Io/IoIosArrowUp'
-import { IoIosArrowDown } from '@react-icons/all-files/Io/IoIosArrowDown'
-import { BsPlusCircle } from '@react-icons/all-files/Bs/BsPlusCircle'
-import { FiMinusCircle } from '@react-icons/all-files/Fi/FiMinusCircle'
+// import { IoIosArrowUp } from '@react-icons/all-files/Io/IoIosArrowUp'
+// import { IoIosArrowDown } from '@react-icons/all-files/Io/IoIosArrowDown'
+import { IoIosArrowUp, IoIosArrowDown } from 'react-icons/Io'
+import { BiPlus } from '@react-icons/all-files/bi/BiPlus'
+import { BiMinus } from '@react-icons/all-files/bi/BiMinus'
 
 class ReservationForm extends React.Component{
   constructor(props){
@@ -16,10 +17,13 @@ class ReservationForm extends React.Component{
   }
     this.handleSubmit = this.handleSubmit.bind(this);
     // this.toggleState = this.toggleState(this);
+    this.getNumGuests = this.getNumGuests.bind(this);
+
   }
 
   componentDidMount() {
     this.toggleState = this.toggleState.bind(this);
+    // this.getNumGuests = this.getNumGuests.bind(this);
 
   }
 
@@ -64,16 +68,28 @@ class ReservationForm extends React.Component{
   }
  }
 
- getNumGuests(operation) {
-  while (this.state.guests < this.props.listing.guests && guests > 0){
-    if(operation === 'add'){
-      this.setState({ guests: this.state.guests += 1 }) 
-    }else{
-      if(operation === 'subtract'){
-        this.setState({ guests: this.state.guests -= 1 }) 
-    }}
+//  getNumGuests(operation) {
+//     while (this.state.guests < this.props.listing.guests && this.state.guests > 0){
+//       if(operation === 'add'){
+//         this.setState({ guests: this.state.guests + 1 }) 
+//       }else{
+//         if(operation === 'subtract'){
+//           this.setState({ guests: this.state.guests - 1 }) 
+//       }}
+//     }
+//  }
+
+  getNumGuests(operation) {
+    const { guests } = this.state;
+    const { listing } = this.props;
+
+    if (operation === 'add' && guests < listing.guests) {
+      this.setState({ guests: guests + 1 });
+    } else if (operation === 'subtract' && guests > 1) {
+      this.setState({ guests: guests - 1 });
+    }
   }
- }
+
 
  toggleState() {
   this.setState({clicked: !this.state.clicked})
@@ -133,11 +149,23 @@ class ReservationForm extends React.Component{
               </div>
           </div>
 
-          {this.state.clicked && <div id='add-subtract-guest-dropdown-content'>
-            dropdown content
-          </div> }
+          { this.state.clicked && <div id='add-subtract-guest-dropdown-content'>
+            
+            <div className='add-subtract-guest-dropdown-div'>
+              <h2 className='add-subtract-guest-dropdown-label'>Guests</h2>
+              <div className='add-subtract-guest-dropdown-guest-adjust'>
+                <BiMinus className='subtract-guests-button' onClick={() => this.getNumGuests('subtract')} />
+              <span className='add-subtract-guests-counter'>{this.state.guests}</span>
+              <BiPlus className='add-guests-button' onClick={() => this.getNumGuests('add')} />
+              </div>
+              
+            </div>
+            
+
+          </div> } 
+            
           
-          <input type="submit" value={this.state.reservation_start && this.state.reservation_end ? 'RESERVE' : 'CHECK AVAILABILITY'}  className='reservation-form-submit'/>
+          <input type="submit" value={this.state.reservation_start && this.state.reservation_end ? 'Reserve' : 'Check availability'}  className='reservation-form-submit'/>
         </form>
         <div className='reservation_form_pricing_breakdown'>
           <div className='reservation_form_pernight_pricing'>
